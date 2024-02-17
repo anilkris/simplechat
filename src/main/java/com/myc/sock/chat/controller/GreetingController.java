fc47bf49de5c9958a5cd6c1b8f2b30801b6d291c
anil <anil@aztik.com>
2024-02-17 18:57:19 +0530

    updated code

---
+++ src/main/java/com/myc/sock/chat/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.myc.sock.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.myc.sock.chat.model.Greeting;
+import com.myc.sock.chat.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+
+  @MessageMapping("/hello")
+  @SendTo("/topic/greetings")
+  public Greeting greeting(HelloMessage message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+    
+}
---
+++ src/main/java/com/myc/sock/chat/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.myc.sock.chat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Greeting {
+
+    private String content;
+    
+}
